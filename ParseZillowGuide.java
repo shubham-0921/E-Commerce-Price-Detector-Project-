0700b6c8337144f0d5dd358badc7972f60e27d7e
shubham-0921 <66564769+shubham-0921@users.noreply.github.com>
2020-06-08 20:00:28 +0530

    Updated Code

--- ParseZillowGuide.java
+++ ParseZillowGuide.java
@@ -18,7 +18,7 @@ public class ParseZillowGuide {
 		try {
 			//Get Document object after parsing the html from given url.
 			document = Jsoup.connect("https://www.amazon.in/ref=gw_pc_Amazondevices_1/dp/B07PFFMP9P?pf_rd_r=MCCRAMTXA92C97DF7KGF&pf_rd_p=4a34e4c6-09f1-49fb-8282-8f136eebde44").get();
-			
+				
 			//Get title
 			String title = document.title();
 			
@@ -31,7 +31,8 @@ public class ParseZillowGuide {
 			
 			System.out.println(title);
 			
-			
+			//Problem 
+			//The below statement is not being run 
 			for (int i=0; i < price.size(); i++) {
 					System.out.println("here");
 					System.out.print(price.get(i).text());
