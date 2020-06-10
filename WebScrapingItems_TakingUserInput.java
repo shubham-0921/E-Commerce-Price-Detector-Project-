2c6632f3976849842829f93f9da8adbf8b43fbeb
shubham-0921 <66564769+shubham-0921@users.noreply.github.com>
2020-06-10 23:53:51 +0530

    Added Snapdeal option

--- WebScrapingItems_TakingUserInput.java
+++ WebScrapingItems_TakingUserInput.java
@@ -1,5 +1,6 @@
 package myJSoupProject;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -13,8 +14,10 @@ public class WebScrapingItems_TakingUserInput {
 		print("running...");
 		Document document1;
 		Document document2;
+		Document document3;
 		
 		Scanner sc=new Scanner(System.in);
+		FileWriter fw=new FileWriter("output.txt");
 		
 		try {
 			//Get Document object after parsing the html from given url.
@@ -26,17 +29,24 @@ public class WebScrapingItems_TakingUserInput {
 			url1=sc.next();
 			
 			document1 = Jsoup.connect(url1).get();
-		
+			
 			//Get title
 			String title1 = document1.title();
 			System.out.println(title1);
+			fw.write(title1);
+			fw.write(',');
+			
+			
 			
 			//Get price1
 			Elements price1=document1.getElementsByClass("_1vC4OE _3qQ9m1");  //To get the price of an item we just need to put that class name which contains the price
 			
 			for(Element elements : price1) {
 				System.out.println(elements.text());
+				fw.write(elements.text());
 			}
+			
+			fw.write(',');
 			
 			
 			//For Amazon 
@@ -50,16 +60,47 @@ public class WebScrapingItems_TakingUserInput {
 			String title2 = document2.title();
 			System.out.println(title2);
 			
+			fw.write(title2);
+			fw.write(',');
+			
 			//Get price2
 			Elements price2=document2.getElementsByClass("a-size-medium a-color-price priceBlockBuyingPriceString");  //To get the price of an item we just need to put that class name which contains the price
 			
 		
 			for(Element elements : price2) {
 				System.out.println(elements.text());
+				fw.write(elements.text());
 			}	
-						
-		
 			
+			fw.write(',');
+			
+			
+			//For Snapdeal
+			String url3 = null;
+			System.out.println("Enter the Snapdeal product URL");
+			url3=sc.next();
+			
+			document3 = Jsoup.connect(url3).get();
+			
+			//Get title
+			String title3 = document3.title();
+			System.out.println(title3);
+			fw.write(title3);
+			fw.write(',');
+			
+			
+			
+			//Get price3
+			Elements price3=document3.getElementsByClass("payBlkBig");  //To get the price of an item we just need to put that class name which contains the price
+			
+			for(Element elements : price3) {
+				System.out.println(elements.text());
+				fw.write(elements.text());
+			}
+			
+			fw.write(',');
+		
+			fw.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
